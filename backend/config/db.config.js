98047d728da2ba8016f0ddcbb3cf7f6aa4635e67
shubhsJadhav95 <shubhamjadhav952004@gmail.com>
2026-06-13 16:00:12 +0530

    k8s database update

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false,
   }
