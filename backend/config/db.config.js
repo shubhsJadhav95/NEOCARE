909e7ceaf4505b26f298a696ccf4214ed1c4285c
shubhsJadhav95 <shubhamjadhav952004@gmail.com>
2026-06-19 01:18:29 +0530

    main branch changes postgres

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
     logging: false, // Set to true if you want to see SQL queries in terminal
   }
 );
